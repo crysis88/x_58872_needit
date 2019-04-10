618cc33e08d00141f9cefa913dbb1f78abb88793
Sandeep Verma <sverma122013@gmail.com>
2019-04-10 10:50:53 +0000

    Changes by 'undefined' on 'NeedIt – 1.0.0' -- Build #1

---
+++ sn/NeedIt/Dictionary Entry/x_58872_needit_needit/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : NeedIt
+ * ClassName   : sys_dictionary
+ * Created On  : 2017-01-24 22:46:34
+ * Created By  : admin
+ * Updated On  : 2019-03-05 07:41:10
+ * Updated By  : system
+ * URL         : /sys_dictionary.do?sys_id=163b7d74db003300a53cfbef29961955
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
