54fed3ee8c54792ab60d49b8234783d2f2c78c18
System Administrator <sverma122013@gmail.com>
2019-04-15 09:43:07 +0000

    Changes by 'System Administrator' on 'NeedIt – 1.0.0' -- Build #1

--- sn/NeedIt/Business Rule/x_58872_needit_needit/active_true/before/Email Address Syntax Validate/script.js
+++ sn/NeedIt/Business Rule/x_58872_needit_needit/active_true/before/Email Address Syntax Validate/script.js
@@ -3,7 +3,7 @@
  * ClassName   : sys_script
  * Created On  : 2017-03-08 03:44:46
  * Created By  : admin
- * Updated On  : 2017-03-08 03:44:46
+ * Updated On  : 2019-04-15 09:34:23
  * Updated By  : admin
  * URL         : /sys_script.do?sys_id=01e885f69f513200bde5f79ff57fcf52
  */
@@ -12,6 +12,7 @@
     // Pass the Requested for email to the Script Include.  Store the return
     // value from the Script Include in the isEmail variable
     var isEmail = validateEmailAddress(current.u_requested_for_email);
+gs.print(isEmail);
     // If isEmail is false (email address syntax is not valid) do not save
     // the record.  Write an error message to the screen.
     if(isEmail == false){
---
+++ sn/NeedIt/Dictionary Entry/x_58872_needit_needit/active_true/test/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : NeedIt
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-04-15 06:45:45
+ * Created By  : admin
+ * Updated On  : 2019-04-15 06:45:45
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=2587b4cadb703300a53cfbef2996192b
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
