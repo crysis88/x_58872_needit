e54e6277829d58f9d36ffd8eb465a76a9fa16dbd
System Administrator <sverma122013@gmail.com>
2019-04-15 06:50:00 +0000

    Changes by 'System Administrator' on 'NeedIt – 1.0.0' -- Build #1

--- sn/NeedIt/Script Include/active_true/x_58872_needit.TestScriptInclude.js
+++ sn/NeedIt/Script Include/active_true/x_58872_needit.TestScriptInclude.js
@@ -3,7 +3,7 @@
  * ClassName   : sys_script_include
  * Created On  : 2019-03-11 10:33:35
  * Created By  : admin
- * Updated On  : 2019-03-12 09:06:55
+ * Updated On  : 2019-04-15 06:43:48
  * Updated By  : admin
  * URL         : /sys_script_include.do?sys_id=d0bb5db6db803300a53cfbef29961944
  */
@@ -17,6 +17,7 @@ TestScriptInclude.prototype = {
 
         // If the testdate is before rightnow, return true, else return false
         if (testdate.before(rightnow)) {
+gs.log('Return now');
             return true;
         }
         else {
